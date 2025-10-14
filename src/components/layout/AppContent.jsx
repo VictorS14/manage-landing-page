7a4c5dbc990c2ed343f2e2dd99529a2f6beb25cb
Victor Moura <victormoura310@gmail.com>
2025-10-14 20:49:28 -0300

    first commit

---
+++ src/components/layout/AppContent.jsx
@@ -0,0 +1,10 @@
+import { Header } from "./Header"
+
+export const AppContent = () => {
+  return (
+    <div className="w-full h-full">
+        <Header />
+    </div>
+  )
+}
+                                      
