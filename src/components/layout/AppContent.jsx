debe0fbb4b8e73eab4ebf5735cd182f9731ff6a0
Victor Moura <victormoura310@gmail.com>
2025-10-15 11:50:29 -0300

    feat: componetiza o elemnto <main>

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,14 +1,11 @@
 import { Header } from "./Header";
-import { SectionIntroduction } from "./SectionIntroduction";
+import { MainContent } from "./MainContent";
 
 export const AppContent = () => {
   return (
-    <div className="w-full min-h-screen bg-orange-50">
+    <div className="w-full min-h-screen bg-orange-50 pb-8">
       <Header />
-
-      <main className=" w-full px-5 pt-3 flex flex-col items-center">
-        <SectionIntroduction />
-      </main>
+      <MainContent />
     </div>
   );
 };
