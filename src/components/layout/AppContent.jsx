38cdc0ed6b735fbc3ca6363de46a01f85a35a78f
Victor Moura <victormoura310@gmail.com>
2025-10-14 21:32:09 -0300

    feat: criar AppComponent

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,8 +1,8 @@
 import { Header } from "./Header"
 
-export const AppContent = () => {
+export const AppContent = () => {          
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-orange-50">
         <Header />
     </div>
   )
