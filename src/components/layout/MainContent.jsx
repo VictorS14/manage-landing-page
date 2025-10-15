fd82fbb5e156052843e214bbf80268d828a3919c
Victor Moura <victormoura310@gmail.com>
2025-10-15 10:18:03 -0300

    feat: criar component MainContent

---
+++ src/components/layout/MainContent.jsx
@@ -0,0 +1,9 @@
+import { SectionIntroduction } from "./SectionIntroduction"
+
+export const MainContent = () => {
+  return (
+    <main className=" w-full px-5 pt-3 flex flex-col items-center">
+        <SectionIntroduction />
+    </main>
+  )
+}
