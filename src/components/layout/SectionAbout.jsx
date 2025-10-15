9b7924089fdb7acddb56e08aae5734af98bcde0f
Victor Moura <victormoura310@gmail.com>
2025-10-15 11:43:45 -0300

    Criar seção SectionAbout

---
+++ src/components/layout/SectionAbout.jsx
@@ -0,0 +1,13 @@
+export const SectionAbout = () => {
+  return (
+    <section className="border px-4">
+      <div className="flex flex-col gap-5 items-center">
+        <h2 className="max-w-60 text-blue-950 text-3xl font-bold text-center">What's different about Manage?</h2>
+        <p  className="text-center text-gray-500">
+          Manage provides all the functionality your team needs, without the complexity. Our software is
+          tailor-made for modern digital product teams.
+        </p>
+      </div>
+    </section>
+  );
+};
