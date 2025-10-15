08987e7224bcc6719e4662cc86360c5f81bc53f9
Victor Moura <victormoura310@gmail.com>
2025-10-14 22:50:05 -0300

    feat: adicionar o main content

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,10 +1,14 @@
-import { Header } from "./Header"
+import { Header } from "./Header";
+import { SectionIntroduction } from "./SectionIntroduction";
 
-export const AppContent = () => {          
+export const AppContent = () => {
   return (
-    <div className="w-full h-full bg-orange-50">
-        <Header />
+    <div className="w-full min-h-screen bg-orange-50">
+      <Header />
+
+      <main className=" w-full px-5 pt-3 flex flex-col items-center">
+        <SectionIntroduction />
+      </main>
     </div>
-  )
-}
-                                      
+  );
+};
