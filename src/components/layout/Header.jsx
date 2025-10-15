032f60d37f0af769a4d536ad0c7eaf6dd4e3d8cc
Victor Moura <victormoura310@gmail.com>
2025-10-14 21:29:36 -0300

    feat: criar Header

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,21 @@
+import logo from '../../assets/images/logo.svg'
+import iconHamburger from '../../assets/images/icon-hamburger.svg'
+import { Menu } from 'lucide-react'
+
 export const Header = () => {
   return (
-    <div className="bg-gray-400">
-        Header
+    <div className="h-20 flex items-center justify-between px-5">
+        <img
+        className='h-4' 
+        src={logo} alt="logo manage" 
+        />
+
+        <button>
+          <img
+        className='h-4' 
+        src={iconHamburger} alt="logo manage" 
+        />
+        </button>
     </div>
   )
 }
