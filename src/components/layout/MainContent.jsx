0e97c78738b2af0dfb71f66ec27e93c67a2bdf33
Victor Moura <victormoura310@gmail.com>
2025-10-15 11:49:08 -0300

    feat: Adiciona o SectionAbout ao MainContent

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,9 +1,11 @@
 import { SectionIntroduction } from "./SectionIntroduction"
+import {SectionAbout} from "./SectionAbout"
 
 export const MainContent = () => {
   return (
-    <main className=" w-full px-5 pt-3 flex flex-col items-center">
+    <main className=" w-full px-5 pt-3 flex flex-col gap-10 items-center">
         <SectionIntroduction />
+        <SectionAbout />
     </main>
   )
 }
