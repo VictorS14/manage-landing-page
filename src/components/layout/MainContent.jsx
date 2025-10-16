82c54f0643ebe8f351f9a675c6323a978fd8b1eb
Victor Moura <victormoura310@gmail.com>
2025-10-16 20:31:18 -0300

    style: Adiciona padding-bottom

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -2,10 +2,11 @@ import { SectionIntroduction } from "./SectionIntroduction"
 import {SectionAbout} from "./SectionAbout"
 import { TipsOfManage } from "./TipsOfManage"
 import { SectionTestimonials } from "./SectionTestimonials"
+import { CallToAction } from "./CallToAction"
 
 export const MainContent = () => {
   return (
-    <main className=" w-full px-5 pt-3 flex flex-col gap-10 items-center">
+    <main className="w-full px-5 pt-3 flex flex-col gap-10 pb-5 items-center">
         <SectionIntroduction />
         <SectionAbout />
         <TipsOfManage />
