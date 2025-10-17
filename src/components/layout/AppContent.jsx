0f87fe2731dd4c6564a570dee7958acc9d334ed8
Victor Moura <victormoura310@gmail.com>
2025-10-16 21:31:57 -0300

    feat: implementa o Footer no AppContent

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -1,13 +1,15 @@
 import { CallToAction } from "./CallToAction";
+import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { MainContent } from "./MainContent";
 
 export const AppContent = () => {
   return (
-    <div className="w-full min-h-screen bg-orange-50 pb-8">
+    <div className="w-full min-h-screen bg-orange-50">
       <Header />
       <MainContent />
       <CallToAction />
+      <Footer />
     </div>
   );
 };
