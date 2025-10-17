2613341237b696e5e128d4bd336c073a0df96f12
Victor Moura <victormoura310@gmail.com>
2025-10-16 21:33:42 -0300

    style: remove border

--- src/components/layout/SectionAbout.jsx
+++ src/components/layout/SectionAbout.jsx
@@ -1,6 +1,6 @@
 export const SectionAbout = () => {
   return (
-    <section className="border px-4">
+    <section className="px-4">
       <div className="flex flex-col gap-5 items-center">
         <h2 className="max-w-60 text-blue-950 text-3xl font-bold text-center">What's different about Manage?</h2>
         <p  className="text-center text-gray-500">
