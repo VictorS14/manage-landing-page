0bdf9bd99fb9974ecdee5195b2f61c9811fbb6db
Victor Moura <victormoura310@gmail.com>
2025-10-17 11:20:57 -0300

    style: Ajusta o layout

--- src/components/layout/SectionAbout.jsx
+++ src/components/layout/SectionAbout.jsx
@@ -1,13 +1,16 @@
+import { TipsOfManage } from "./TipsOfManage";
+
 export const SectionAbout = () => {
   return (
-    <section className="px-4">
-      <div className="flex flex-col gap-5 items-center">
-        <h2 className="max-w-60 text-blue-950 text-3xl font-bold text-center">What's different about Manage?</h2>
-        <p  className="text-center text-gray-500">
+    <section className="flex flex-col gap-12 lg:w-full lg:flex-row lg:justify-between">
+      <div className="flex flex-col gap-5 items-center lg:items-start">
+        <h2 className="max-w-60 lg:max-w-lg text-blue-950 text-[min(7vw,40px)] font-bold text-center lg:text-start">What's different about Manage?</h2>
+        <p  className="text-center lg:text-start lg:max-w-80 text-gray-500">
           Manage provides all the functionality your team needs, without the complexity. Our software is
           tailor-made for modern digital product teams.
         </p>
       </div>
+      <TipsOfManage />
     </section>
   );
 };
