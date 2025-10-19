c11aedd77ca436bd776ff88402e57791e67bdf9f
Victor Moura <victormoura310@gmail.com>
2025-10-18 23:35:19 -0300

    feat: Recebe props para minupular o ModalOverlay

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,21 +1,22 @@
 import logo from "../../assets/images/logo.svg";
 import iconHamburger from "../../assets/images/icon-hamburger.svg";
+import iconClose from "../../assets/images/icon-close.svg";
 import { Button } from "../ui/Button";
 import { NavBarLinks } from "./NavBarLinks";
 
-export const Header = () => {
+export const Header = ({modalIsOpen, handleClick}) => {
   return (
     <div className="h-20 flex items-center justify-between px-5 w-full max-w-7xl ">
       <img className="h-4" src={logo} alt="logo manage" />
 
-      <button className="lg:hidden">
-        <img className="h-4" src={iconHamburger} alt="logo manage" />
+      <button className="lg:hidden" onClick={handleClick}>
+        <img className="h-4" src={modalIsOpen ? iconClose : iconHamburger} alt="logo manage" />
       </button>
 
       <div className="w-2xl hidden lg:flex">
-        <NavBarLinks />
+        <NavBarLinks className={"w-full flex items-center justify-center gap-8"}/>
       </div>
-      
+
       <Button
         text={"Get Started"}
         className={
