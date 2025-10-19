d4513c23d91b4e901da5a6852722e80b1f2e9ddd
Victor Moura <victormoura310@gmail.com>
2025-10-18 23:36:47 -0300

    Implementa o ModalOverlay no MainContent 	ignorei o prop drilling pois não achei necessário 	usar Context pra passar apenas duas props :)

--- src/components/layout/MainContent.jsx
+++ src/components/layout/MainContent.jsx
@@ -1,13 +1,22 @@
-import { SectionIntroduction } from "./SectionIntroduction"
-import {SectionAbout} from "./SectionAbout"
-import { SectionTestimonials } from "./SectionTestimonials"
+import { SectionIntroduction } from "./SectionIntroduction";
+import { SectionAbout } from "./SectionAbout";
+import { SectionTestimonials } from "./SectionTestimonials";
+import { ModalOverlay } from "../ui/ModalOverlay";
+import { ModalMenu } from "../ui/ModalMenu";
 
-export const MainContent = () => {
+export const MainContent = ({ modalIsOpen, handleClick }) => {
   return (
-    <main className="w-full max-w-7xl px-5 pt-3 flex flex-col gap-10 lg:gap-18 pb-5 items-center border">
-        <SectionIntroduction />
-        <SectionAbout />
-        <SectionTestimonials />
+    <main className="relative w-full max-w-7xl px-5 pt-3 flex flex-col gap-10 lg:gap-18 pb-5 items-center">
+      <SectionIntroduction />
+      <SectionAbout />
+      <SectionTestimonials />
+      <ModalOverlay
+        // Yeess i know about prop drilling
+        handleClick={handleClick}
+        modalIsopen={modalIsOpen}
+      >
+        <ModalMenu />
+      </ModalOverlay>
     </main>
-  )
-}
+  );
+};
