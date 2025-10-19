55d514fc2915df82f0247cf537a71b5b950d532e
Victor Moura <victormoura310@gmail.com>
2025-10-18 23:32:42 -0300

    feat: Cria o estado para manipular o ModalOverlay

--- src/components/layout/AppContent.jsx
+++ src/components/layout/AppContent.jsx
@@ -3,11 +3,23 @@ import { Footer } from "./Footer";
 import { Header } from "./Header";
 import { MainContent } from "./MainContent";
 
+import { useState } from "react";
+
 export const AppContent = () => {
+  const [modalIsOpen, setModalIsopen] = useState(false);
+
+  const handleClick = () => {
+    setModalIsopen(!modalIsOpen)
+  }
+
   return (
     <div className="w-full min-h-screen bg-orange-50 flex flex-col items-center">
-      <Header />
-      <MainContent />
+      <Header 
+        modalIsOpen={modalIsOpen} 
+        handleClick={handleClick} />
+      <MainContent
+        handleClick={handleClick} 
+        modalIsOpen={modalIsOpen}/>
       <CallToAction />
       <Footer />
     </div>
